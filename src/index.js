ad46710b0889c0c0a0fb0aaa844b32bbe9e65e55
danipo3345 <153043568+danipo3345@users.noreply.github.com>
2025-12-12 08:34:42 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
+// Use "type: commonjs" in package.json to use CommonJS modules
 const express = require('express');
-const bodyParser = require("body-parser");
 const cors = require('cors');
-const path = require("path");
-
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs");
-// ajusta o caminho absoluto da pasta views
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs')
+app.set('views', "./views")
 
-app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-// rotas
-require('../db/dbconnect')(app);
-require('../route/home')(app);
-require('../route/getnoticias')(app);
-require('../route/getidnoticias')(app);
-require('../route/gettiponoticias')(app);
-require('../route/delnoticias')(app);
-require('../route/postnoticias')(app);
-require('../route/putnoticias')(app);
+require("../db/db")(app);
+require("../rotas/home")(app);
+require("../rotas/getnoticias")(app);
+require("../rotas/getidnoticias")(app);
+require("../rotas/gettiponoticias")(app);
+require("../rotas/putnoticias")(app);
+require("../rotas/postnoticias")(app);
+require("../rotas/delnoticias")(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
